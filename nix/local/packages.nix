1de9ac0bda72e7ab8e996cbeecb242e0980fc1b2
whs <hswongac@gmail.com>
2024-02-06 07:59:43 +0800

    Update

--- nix/local/packages.nix
+++ nix/local/packages.nix
@@ -69,9 +69,27 @@ with inputs.nixpkgs; let
       done
     '';
   };
+  junod_19_1_0 = buildGoModule rec {
+    pname = "junod";
+    version = "19.1.0";
+    src = fetchurl {
+      url = "https://github.com/CosmosContracts/juno/archive/refs/tags/v${version}.tar.gz";
+      sha256 = "sha256-q4KrO69oSKgBIeyr+dSvxCH+8LmmVO6x1SeYstRXY8Y=";
+    };
+    vendorHash = "sha256-gVdB0qjArLGI+HJgymrHBjbb10olUDqNbG/xKpVpfHM=";
+    subPackages = "cmd/junod";
+    buildInputs = [libwasmvm_1_5_2];
+    postInstall = ''
+      for f in "$out"/bin/*; do
+        local nrp="$(patchelf --print-rpath "$f" | sed -E 's@(:|^)'$NIX_BUILD_TOP'[^:]*:@\1@g')"
+        patchelf --set-rpath "$nrp" "$f"
+      done
+    '';
+  };
 in {
   inherit libwasmvm_1_5_1;
   inherit libwasmvm_1_5_2;
   inherit junod_18_1_0;
   inherit junod_19_0_0;
+  inherit junod_19_1_0;
 }
