76db8ce5f58eff2084528b210c08465f6850c3ed
whs <hswongac@gmail.com>
2025-05-02 20:08:50 +0800

    Update

--- nix/local/packages.nix
+++ nix/local/packages.nix
@@ -6,25 +6,17 @@ with inputs.nixpkgs; let
     done
   '';
 
+  postFixup = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/junod
+    chmod +x $out/bin/junod
+  '';
+
   libwasmvm_1_5_8 = stdenv.mkDerivation rec {
     pname = "libwasmvm";
     version = "1.5.8";
     src = fetchurl {
       sha256 = "sha256-xFgTY+tvSrKrpfgLBfuUDd7Jkp7LzX/0vgxSGpPsfo4=";
-      url = "https://github.com/CosmWasm/wasmvm/releases/download/v${version}/libwasmvm.x86_64.so";
-    };
-    dontBuild = true;
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out/lib
-      cp $src $out/lib/libwasmvm.x86_64.so
-    '';
-  };
-  libwasmvm_2_2_2 = stdenv.mkDerivation rec {
-    pname = "libwasmvm";
-    version = "2.2.2";
-    src = fetchurl {
-      sha256 = "sha256-xtVXsOrNX+vlNWuxyh0JhJzjXbH5+tTvMQL5uslv8NY=";
       url = "https://github.com/CosmWasm/wasmvm/releases/download/v${version}/libwasmvm.x86_64.so";
     };
     dontBuild = true;
@@ -46,17 +38,20 @@ with inputs.nixpkgs; let
     buildInputs = [libwasmvm_1_5_8];
     inherit postInstall;
   };
-  junod_28_0_2 = buildGoModule rec {
+  junod_28_0_2 = stdenv.mkDerivation rec {
     pname = "junod";
     version = "28.0.2";
     src = fetchurl {
-      sha256 = "sha256-K5KziwhRlBjZPQF+MoW5jQdXIhxQWcxjG2HFgZfEiHE=";
-      url = "https://github.com/CosmosContracts/juno/archive/refs/tags/v${version}.tar.gz";
+      url = "https://github.com/CosmosContracts/juno/releases/download/v${version}/junod";
+      sha256 = "sha256-7x8AcT5msL3qUDc074cvFaNaBNKOGGdOZB8D/jVoNZc=";
     };
-    vendorHash = "sha256-Fd1o4vt9Q0Oc2DT4Yh8A1QM0lqfoBhDggOopvrDkr44=";
-    subPackages = "cmd/junod";
-    buildInputs = [libwasmvm_2_2_2];
-    inherit postInstall;
+    dontUnpack = true;
+    dontBuild = true;
+    dontConfigure = true;
+    dontInstall = true;
+    dontPatch = true;
+    dontPatchELF = true;
+    inherit postFixup;
   };
 in {
   inherit libwasmvm_1_5_8;
